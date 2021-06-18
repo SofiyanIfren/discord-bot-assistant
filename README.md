6dac053d92e471cf1e99615a99ef783788021a8c
sofiyan <ifren.sofiyan@gmail.com>
2021-06-18 23:52:02 +0200

    ajout proverbes

---
+++ README.md
@@ -0,0 +1,11 @@
+# Installation npm et packages nécessaires
+
+<code>npm init
+npm i --save  discord.js ytdl-core ffmpeg fluent-ffmpeg ffmpeg-static @discordjs/opus ytdl-core</code>
+
+# Paramétrage des informations de connexion
+## Créer un fichier config.json à la racine du projet
+
+{
+    "BOT_TOKEN": "<TOKEN_DU_BOT_DISCORD"
+}
--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// idées de fonctionnalités (!help pour la liste) : recherche !wiki, !gif api, !youtube query (!play/!pause/!resume), 
+// !convert (binaire, ASCII, hex, octal, RGB conversions)
+// hash/decrypt => MD5, SHA1, SHA256, SHA512 and other hashes / MD5 and SHA1 decrypt search
+// ! trad (eng/fr, see libretranslate) 
+// currency converter / crypto converter
+// joke api (random)
+// citations inspirantes
+// mini-games
+const proverbes = require("./proverbes.json")
+
 const Discord   = require("discord.js")
 const config    = require("./config.json")
 const client    = new Discord.Client()
@@ -5,7 +15,7 @@ const ytdl      = require('ytdl-core')
 
 const prefix = '!'
 
-client.on('ready', () => console.log('Hello World, I\'m awaken!'))
+client.on('ready', () => console.log('Hello World, I\'m awaken!')) // 
 
 client.on('message', async message => {
     if (message.author.bot) return // it's not a bot who's speaking
@@ -16,19 +26,17 @@ client.on('message', async message => {
     const command       = args.shift().toLowerCase()
 
     if (command === 'ping'){
-        const timeTaken = Date.now() - message.createdTimestamp;
+        const timeTaken = Date.now() - message.createdTimestamp
         message.reply(`Pong! This message had a latency of ${timeTaken} ms.`)
-    } 
-    
-    
-    else if (command === 'despacito'){
-        //message.reply(`Hi there ! What can I do for you ?`) 
-        voiceChannel = client.channels.cache.get('776491312814882867') // coworking
-        //console.log(voiceChannel)
+    } else if (command === 'proverbe'){
+        const proverbe = proverbes[Math.floor(Math.random() * proverbes.length)]
+        message.reply(`${Object.keys(proverbe)[0]} - ${Object.values(proverbe)[0]}`)
+    } else if (command === 'despacito'){
+        voiceChannel = client.channels.cache.get('776491312814882867') // coworking channel id
         const connection = await voiceChannel.join()
         connection.play(await ytdl('https://www.youtube.com/watch?v=kJQP7kiw5Fk', { type: 'opus' }));
     }
 })
 
 
-client.login(config.BOT_TOKEN) // .then(() => console.log(client.channels.cache.array()))
+client.login(config.BOT_TOKEN)
